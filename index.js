6723d356bf44e141978014a8fb4410dcf6c171f4
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-04-01 09:44:52 -0400

    added badges and license text

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const questions = require("./questions.js");
 const inquirer = require("inquirer");
-//this is a comment
+const licenseText = require("./license.js")
+const badgeIcon = require("./badges.js")
+let badge;
 
 const generateHTML = (answers) =>
 `# Table of Contents:
@@ -17,6 +19,9 @@ const generateHTML = (answers) =>
 # Title:
 # ${answers.title}
 
+# Owned By:
+${answers.name}
+
 ## Description:
 ${answers.description}
 
@@ -30,27 +35,7 @@ ${answers.contribution}
 Some of the tests that I ran for this project are: ${answers.tests}.
 
 #License
-MIT License
-
-Copyright (c) 2021 ${answers.name}
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+${answers.license}
 
 #Github
 Please visit my ${answers.github} to visit my work.
